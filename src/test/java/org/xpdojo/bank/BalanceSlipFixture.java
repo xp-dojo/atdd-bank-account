66aa14b096e56510ab406f5f4678af0825ba7c4a
Toby <toby.weston@gmail.com>
2019-03-03 19:49:40 +0000

    initial commit of project structure, working maven pom and gradle build

---
+++ src/test/java/org/xpdojo/bank/BalanceSlipFixture.java
@@ -0,0 +1,8 @@
+package org.xpdojo.bank;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class BalanceSlipFixture {
+}
