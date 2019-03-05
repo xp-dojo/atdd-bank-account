aa68dd81a17f8d2c63dc9707ad6eb4fed8b95ee0
Toby <toby.weston@gmail.com>
2019-03-05 20:52:05 +0000

    should we deal with big numbers? #4

--- src/test/java/org/xpdojo/bank/MoneyTest.java
+++ src/test/java/org/xpdojo/bank/MoneyTest.java
@@ -1,13 +1,15 @@
 package org.xpdojo.bank;
 
-import org.hamcrest.CoreMatchers;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static com.sun.jmx.snmp.ThreadContext.contains;
-import static org.hamcrest.CoreMatchers.*;
+import static java.lang.Long.*;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.xpdojo.bank.Money.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.xpdojo.bank.Money.amountOf;
 
 class MoneyTest {
 
@@ -19,6 +21,19 @@ class MoneyTest {
 	@Test
 	void addingNegativeMonies() {
 		assertThat(amountOf(10).plus(amountOf(-14)), is(amountOf(-4)));
+	}
+
+	@Test
+	@Disabled
+	void addingBigMoneySafely() {
+		assertThat(amountOf(MAX_VALUE / 2).plus(amountOf(MAX_VALUE / 2)), is(amountOf(MAX_VALUE)));
+	}
+
+	@Test
+	@Disabled
+	void addingBigMoney() {
+		assertThrows(IllegalArgumentException.class, () -> amountOf(MAX_VALUE).plus(amountOf(1)));
+		assertThrows(IllegalArgumentException.class, () -> amountOf(MAX_VALUE / 2).plus(amountOf((MAX_VALUE / 2) + 1)));
 	}
 
 	@Test
