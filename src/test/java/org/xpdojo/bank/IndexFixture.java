8d1bf0638c4b1c0eb2bed51f5289be3e39be8992
Toby <toby.weston@gmail.com>
2019-03-07 21:44:21 +0000

    apply source styling overrides (i.e concordion.css) to target output

--- src/test/java/org/xpdojo/bank/IndexFixture.java
+++ src/test/java/org/xpdojo/bank/IndexFixture.java
@@ -1,8 +1,10 @@
 package org.xpdojo.bank;
 
+import org.concordion.api.ConcordionResources;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
+@ConcordionResources(value = { "../../../../resources/concordion.css" }, includeDefaultStyling = false)
 public class IndexFixture {
 }
